ed5a3ca12284fd20fdf2286fb4157604185f4845
bolg55 <kellen.bolger@strapi.io>
2025-01-22 11:04:09 -0500

    test: remove cloudinary for test

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -11,27 +11,26 @@ module.exports = ({ env }) => ({
         defaultReplyTo: env("SENDGRID_DEFAULT_TO"),
         testAddress: env("SENDGRID_DEFAULT_FROM"),
       },
-
     },
   },
-  upload: {
-    config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
-      breakpoints:{
-        xlarge: 1920,
-        thumbnail: 100,
-      },
-    },
-  },
+  // upload: {
+  //   config: {
+  //     provider: "cloudinary",
+  //     providerOptions: {
+  //       cloud_name: env("CLOUDINARY_NAME"),
+  //       api_key: env("CLOUDINARY_KEY"),
+  //       api_secret: env("CLOUDINARY_SECRET"),
+  //     },
+  //     actionOptions: {
+  //       upload: {},
+  //       uploadStream: {},
+  //       delete: {},
+  //     },
+  //     breakpoints:{
+  //       xlarge: 1920,
+  //       thumbnail: 100,
+  //     },
+  //   },
+  // },
   // ...
 });
