cf1aa7a193f219da5ce1b0664d947c2a63549ff4
bolg55 <kellen.bolger@strapi.io>
2026-06-04 17:38:08 -0400

    test: experimental release

--- .strapi/client/app.js
+++ .strapi/client/app.js
@@ -4,13 +4,11 @@
  */
 import graphql from "@strapi/plugin-graphql/strapi-admin";
 import usersPermissions from "@strapi/plugin-users-permissions/strapi-admin";
-import strapiPluginRateLimit from "strapi-plugin-rate-limit/strapi-admin";
 import { renderAdmin } from "@strapi/strapi/admin";
 
 renderAdmin(document.getElementById("strapi"), {
   plugins: {
     graphql: graphql,
     "users-permissions": usersPermissions,
-    "strapi-plugin-rate-limit": strapiPluginRateLimit,
   },
 });
